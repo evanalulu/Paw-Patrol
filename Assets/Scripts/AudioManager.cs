e87247a5e6918cdb157dc91bd39e8637c8c55e25
Evana Pradhan <evanapra@gmail.com>
2025-10-29 23:04:50 -0700

    Add background music to gameplay

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+
+    [Header("Audio Sources")]
+    public AudioSource Music;
+    public AudioSource SoundFX;
+
+    [Header("Audio Clips")]
+    public AudioClip BackgroundMusic;
+    public AudioClip AttackSound;
+    public AudioClip CollisionSound;
+    public AudioClip PetCollectSound;
+    public AudioClip WoofSound;
+    public AudioClip MeowSound;
+
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        // background music
+        if (Music != null && BackgroundMusic != null)
+        {
+            Music.clip = BackgroundMusic;
+            Music.loop = true;
+            Music.Play();
+        }
+    }
+
+    public void Play(AudioClip clip)
+    {
+        if (clip != null && SoundFX != null)
+            SoundFX.PlayOneShot(clip);
+    }
+
+    // Randomly play Woof or Meow.
+    public void PlayRandomPetSound()
+    {
+        AudioClip clipToPlay = Random.value > 0.5f ? WoofSound : MeowSound;
+        Play(clipToPlay);
+    }
+}
